83b93e44ce8de3ba90a1c614536882af8f30b8f9
Jamais <lucaasbarros1992@gmail.com>
2025-05-20 10:34:15 -0300

    Project created

---
+++ src/test/java/com/gamesestudo/gamelist/GamelistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamesestudo.gamelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamelistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
